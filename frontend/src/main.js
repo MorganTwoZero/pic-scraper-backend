4f988f4034ca5b3f72d1f31a49238c22f0290f6b
MorganTwoZero <90017582+MorganTwoZero@users.noreply.github.com>
2023-04-17 09:18:28 +0200

    continue to fix the backend url in the frontend

--- frontend/src/main.js
+++ frontend/src/main.js
@@ -15,7 +15,7 @@ import Toast, { POSITION } from "vue-toastification";
 import "vue-toastification/dist/index.css";
 
 axios.defaults.withCredentials = true;
-axios.defaults.baseURL = import.meta.env.VITE_APP_BACKEND_URL;  // backend
+axios.defaults.baseURL = "https://pixiv.sbs/api";  // backend
 
 const app = createApp(App)
 
